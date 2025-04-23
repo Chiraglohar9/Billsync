9dfaf95b9dc02aab4373471bb3750970913375df
Chirag Lohar <loharchirag916@gmail.com>
2025-04-23 17:40:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager
 from Controllers.auth_controller import auth_bp
 from Controllers.user_controller import user_bp
-from Models.dashboard_filter_model import invoice_count
 from configuration import Config
+from flask_mail import Mail
+
 app = Flask(__name__)
 app.config.from_object(Config)
 jwt = JWTManager(app)
-app.register_blueprint(auth_bp, url_prefix='/auth')
+mail = Mail(app)  
+
+app.register_blueprint(auth_bp, url_prefix='/auth') 
 app.register_blueprint(user_bp, url_prefix='/user')
+
 @app.route('/')
 def home():
     if 'jwt_token' not in session:
         return redirect(url_for('auth.login'))
-    return redirect(url_for('user.dashboard'))
+    username = session.get('username')
+    if username is None:
+        return redirect(url_for('auth.login'))
+    return redirect(url_for('dashboard'))
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5006)
+    app.run(debug=True, port=5001)
